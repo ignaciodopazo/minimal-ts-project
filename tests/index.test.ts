b9ff1bcee2853b71d267d6253aab12556a625d22
ignaciodopazo <ignacio.dopazo11@gmail.com>
2022-12-29 21:59:57 -0300

    feat: add linter

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -3,10 +3,14 @@ import {
   expect,
   it,
 } from "@jest/globals";
-import { sum } from "../src";
+import { diff, sum } from "../src";
 
 describe("Tests", () => {
   it("sum", async () => {
     expect(sum(1, 1)).toEqual(2);
+  });
+
+  it("diff", async () => {
+    expect(diff(1, 1)).toEqual(0);
   });
 });
